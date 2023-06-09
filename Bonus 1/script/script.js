404c25a66ce82f7f3cd0438b3bbfafe9df8b95c9
IanzanoLorenzo <ianzano.lorenzo@gmail.com>
2023-06-09 16:21:25 +0200

    bonus done

---
+++ Bonus 1/script/script.js
@@ -0,0 +1,71 @@
+"use strict";
+
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            newTask: '',
+            tasks :[
+                {
+                    text: 'Preparare la colazione',
+                    done: false,
+                    modify: false
+                },
+                {
+                    text: 'Fare palesta',
+                    done: false,
+                    modify: false
+                },
+                {
+                    text: 'Rifare il letto',
+                    done: false,
+                    modify: false
+                },
+                {
+                    text: 'Andare a prendere mio figlio alle elementeri',
+                    done: false,
+                    modify: false
+                },
+                {
+                    text: 'Preparare il pranzo',
+                    done: false,
+                    modify: false
+                },
+                {
+                    text: 'Pulire il salone',
+                    done: false,
+                    modify: false
+                },
+                {
+                    text: 'Fare la spesa per il giorno dopo',
+                    done: false,
+                    modify: false
+                },
+            ]  
+        }
+    },
+    methods: {
+        doneUndoneTask(index){
+            this.tasks[index].done = !this.tasks[index].done
+        },
+        deleteTask(index){
+            this.tasks.splice(index, 1)
+        },
+        addNewTask(){
+            let obj = {
+                text : this.newTask,
+                done : false,
+                modify : false
+            }
+
+            this.tasks.push(obj)
+
+            this.newTask = ''
+        },
+        modifyTask(index){
+            this.tasks[index].modify = !this.tasks[index].modify
+            console.log(this.tasks[index].modify)
+        }
+    },
+}).mount('#app')
