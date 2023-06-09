0a4f2963ac0739e7f7f96824876fcc0db6062fe5
IanzanoLorenzo <ianzano.lorenzo@gmail.com>
2023-06-09 16:09:25 +0200

    exercise done

--- script/script.js
+++ script/script.js
@@ -5,43 +5,63 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-          tasks :[
-            {
-                text: 'Fare la spesa',
-                done: false,
-                modify: false
-            },
-            {
-                text: 'Fare la spesa',
-                done: false,
-                modify: false
-            },
-            {
-                text: 'Fare la spesa',
-                done: false,
-                modify: false
-            },
-            {
-                text: 'Fare la spesa',
-                done: false,
-                modify: false
-            },
-            {
-                text: 'Fare la spesa',
-                done: false,
-                modify: false
-            },
-            {
-                text: 'Fare la spesa',
-                done: false,
-                modify: false
-            },
-            {
-                text: 'Fare la spesa',
-                done: false,
-                modify: false
-            },
-          ]  
+            newTask: '',
+            tasks :[
+                {
+                    text: 'Preparare la colazione',
+                    done: false,
+                    modify: false
+                },
+                {
+                    text: 'Fare palesta',
+                    done: false,
+                    modify: false
+                },
+                {
+                    text: 'Rifare il letto',
+                    done: false,
+                    modify: false
+                },
+                {
+                    text: 'Andare a prendere mio figlio alle elementeri',
+                    done: false,
+                    modify: false
+                },
+                {
+                    text: 'Preparare il pranzo',
+                    done: false,
+                    modify: false
+                },
+                {
+                    text: 'Pulire il salone',
+                    done: false,
+                    modify: false
+                },
+                {
+                    text: 'Fare la spesa per il giorno dopo',
+                    done: false,
+                    modify: false
+                },
+            ]  
+        }
+    },
+    methods: {
+        doneUndoneTask(index){
+            this.tasks[index].done = !this.tasks[index].done
+        },
+        deleteTask(index){
+            this.tasks.splice(index, 1)
+        },
+        addNewTask(){
+            let obj = {
+                text : this.newTask,
+                done : false,
+                modify : false
+            }
+
+            this.tasks.push(obj)
+
+            this.newTask = ''
         }
     },
 }).mount('#app')
